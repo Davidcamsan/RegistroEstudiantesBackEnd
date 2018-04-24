26d369e0bd82315ff0f56fdf6f5e97fd368bc572
David Cambronero <Davidcamsan16@gmail.com>
2018-04-24 15:56:30 -0600

    Initial commit

---
+++ RegistroEstudiantesBackEnd/Controllers/EstudianteController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RegistroEstudiantesBackEnd.Models;
+using Newtonsoft.Json; 
+
+namespace RegistroEstudiantesBackEnd.Controllers
+{
+    public class EstudianteController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+
+        public string GetAllStudents(){
+
+            List<Estudiante> lstEstudiantes = Estudiante.getAllStudents();
+
+            return JsonConvert.SerializeObject(lstEstudiantes);
+        }
+    }
+}
---
+++ RegistroEstudiantesBackEnd/Models/Estudiante.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace RegistroEstudiantesBackEnd.Models
+{
+    public class Estudiante
+    {
+        public Estudiante()
+        {
+        }
+
+        public string Nombre { get; set; }
+        public int ID { get; set; }
+        public string Foto { get; set; }
+        public int Nota { get; set; }
+
+
+        public static List<Estudiante> getAllStudents()
+        {
+            List<Estudiante> lstEstudiantes = new List<Estudiante>();
+
+            lstEstudiantes.Add(new Estudiante { ID = 1, Nombre = "Carlos Dan", Nota = 70, Foto = "https://d30y9cdsu7xlg0.cloudfront.net/png/410776-200.png" });
+            lstEstudiantes.Add(new Estudiante { ID = 1, Nombre = "Carla Dan", Nota = 70, Foto = "https://d30y9cdsu7xlg0.cloudfront.net/png/410776-200.png" });
+            lstEstudiantes.Add(new Estudiante { ID = 1, Nombre = "Sun Dan", Nota = 70, Foto = "https://d30y9cdsu7xlg0.cloudfront.net/png/410776-200.png" });
+
+            return lstEstudiantes;
+
+        }
+    }
+}
